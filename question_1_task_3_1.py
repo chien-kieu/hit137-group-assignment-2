b7c2a419cf965bee69c15a679b5ca7b95bd527b4
chien-kieu <k.quocchien@gmail.com>
2024-01-14 00:30:01 +0930

    Upload question 1 task 3

---
+++ question_1_task_3_1.py
@@ -0,0 +1,24 @@
+import re
+import pandas as pd
+from collections import Counter
+
+output_csv_file = 'top_30_words.csv'
+
+# Read content from the file
+with open('combined_text.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+
+# Use regular expression to extract words
+words = re.findall(r'\b\w+\b', text)
+
+# Count the occurrences of each word
+word_counts = Counter(words)
+
+# Get the top 30 words and their counts
+top_30_words = [(word, count) for word, count in word_counts.most_common(30)]
+
+# Save the top 30 words and their counts to a CSV file
+df_top_30 = pd.DataFrame(top_30_words, columns=['Word', 'Count'])
+df_top_30.to_csv(output_csv_file, index=False)
+
+print(f"The top 30 words and their counts have been saved to {output_csv_file}")
---
+++ question_1_task_3_3.py
@@ -0,0 +1,42 @@
+# Task 3
+from transformers import AutoTokenizer
+from collections import Counter
+import pandas as pd
+
+def analyze_and_save_top_30_tokens(file_path, model_name="bert-base-cased", chunk_size=1000000, top_n=30):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    total_token_counts = Counter()
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        while True:
+            chunk = file.read(chunk_size)
+            if not chunk:
+                break
+
+            # Tokenize the chunk
+            tokens = tokenizer.tokenize(chunk)
+            # Count the tokens in the chunk
+            total_token_counts.update(tokens)
+            
+    # Get the top 30 tokens for the entire file
+    top_30_tokens = total_token_counts.most_common(top_n)
+
+    # Print the top 30 tokens for the entire file
+    print(f"\nTop {top_n} tokens for the entire file:")
+    for token, count in top_30_tokens:
+        print(f"{token}: {count}")
+
+    # Convert top_30_tokens to a DataFrame
+    df_top_30_tokens = pd.DataFrame(top_30_tokens, columns=['Token', 'Count'])
+
+    # Save top_30_tokens to a CSV file
+    csv_file_name = f"top_{top_n}_tokens.csv"
+    df_top_30_tokens.to_csv(csv_file_name, index=False)
+    print(f"\nTop 30 tokens have been saved to '{csv_file_name}'.")
+
+def main():
+    input_file_path = "./combined_text.txt"
+    analyze_and_save_top_30_tokens(input_file_path)
+
+if __name__ == "__main__":
+    main()
